91bc5a8d77f81a21bde120c5e14636a4d8c061c1
Syed Muzammil Ali <muzammilalisyed157@gmail.com>
2024-03-25 14:55:41 +0500

    add

---
+++ index.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+console.log("wellcome to codewithMuzammil -CLI Number Guessing Game");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit 1 to 5):"
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulation ! you guess a correct number.");
+}
+else {
+    console.log("Sorry, you guess a wrong number");
+}
